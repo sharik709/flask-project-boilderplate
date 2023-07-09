395ccffcf258d8f214a80dec260c5dffb0e64784
Sharik Shaikh <shaikhsharik709@gmail.com>
2023-07-09 09:03:35 +0530

    models and db added

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 
 from config import Config
+from app.extensions import db
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     # Initialize Flask extensions here
+    db.init_app(app)
 
     # Register blueprints here
     from app.main import bp as main_bp
@@ -14,6 +16,9 @@ def create_app(config_class=Config):
 
     from app.posts import bp as posts_bp
     app.register_blueprint(posts_bp, url_prefix='/posts')
+
+    from app.questions import bp as questions_bp
+    app.register_blueprint(questions_bp, url_prefix='/questions')
 
     @app.route('/test/')
     def test_page():
